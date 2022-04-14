710048b5c1f1bcdf946f6ef8bbeff634c8b88c7b
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-04-14 01:50:08 -0300

    att

---
+++ tarefas/quiz/script.js
@@ -0,0 +1,11 @@
+const NomeInput = document.getElementById("nome");
+
+let nome;
+
+function VerificarQuiz(){
+    if(){
+        console.log("deu certo")
+    }else {
+        console.log("deu errado")
+    }
+}
