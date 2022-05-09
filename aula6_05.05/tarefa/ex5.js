e610ce96c80a16b8915be45c7b6c60b3c2f7a78c
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-05-09 12:52:29 -0300

    att

--- aula6_05.05/tarefa/ex5.js
+++ aula6_05.05/tarefa/ex5.js
@@ -1,1 +1,12 @@
 //leia 10 nomes e mostre ao usuario a quantidade de letras
+
+let nomes = ["Gabriel", "jose", "Adolfo", "Anselmo", "Isabelas", "Augusto", "Gustavo", "Gabriela", "Giovana", "Matheus"]
+
+let n = nomes.length
+let total = 0
+
+for (let i = 0; i < n; i++){
+    total += nomes[i].length
+    console.log(nomes)
+}
+console.log("Total de Caracteres", total)
