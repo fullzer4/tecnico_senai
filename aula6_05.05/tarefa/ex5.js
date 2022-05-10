d8769207d4bac556259138e40ddb191d87187c83
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-05-09 23:11:46 -0300

    att

--- aula6_05.05/tarefa/ex5.js
+++ aula6_05.05/tarefa/ex5.js
@@ -1,12 +1,10 @@
 function minhalista(...nomes){ // ele deixa inserir mais doq um nome na funcao como lista
     console.log(nomes);
     Qnomes = nomes.length;
-    x = 1;
     TotalC = (nomes.join(''));
     TotalC = TotalC.length
-    if (Qnomes > x){
+    if (Qnomes > 1){
         console.log(`numero de caracteres Total:  ${TotalC}, com um total de ${Qnomes} de nomes`);
     }
 }
-
 minhalista("Matheus", "Gabriel", "Joao", "Gigi", "Isabela", "Roger", "Tulio", "Federico", "Manuela", "PauloBastistella");
