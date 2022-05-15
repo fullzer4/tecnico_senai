ef07481baef36244ebc17b43cdcd48bb9910f522
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-05-15 15:06:38 -0300

    att

--- aula7_12.05/tarefa/matrizD.js
+++ aula7_12.05/tarefa/matrizD.js
@@ -1,22 +1,15 @@
 x1 = Math.floor(Math.random() * 10)
 y1 = Math.floor(Math.random() * 10)
-z1 = Math.floor(Math.random() * 10)
-matriz1 = [x1, y1, z1]
+matriz1 = [x1, y1]
+
 x2 = Math.floor(Math.random() * 10)
 y2 = Math.floor(Math.random() * 10)
-z2 = Math.floor(Math.random() * 10)
-matriz2 = [x2, y2, z2]
-x3 = Math.floor(Math.random() * 10)
-y3 = Math.floor(Math.random() * 10)
-z3 = Math.floor(Math.random() * 10)
-matriz3 = [x3, y3, z3]
+matriz2 = [x2, y2]
 
-matrizP = [matriz1, matriz2, matriz3]
+matrizP = [matriz1, matriz2]
 console.log(matrizP);
 
-
-diagonalP1 = matrizP[0][0] *=2
-diagonalP2 = matrizP[1][1] *=2
-diagonalP3 = matrizP[2][2] *=2
-
-console.log(`Os elementos da diagonal principal multiplicados ficam: ${diagonalP1}, ${diagonalP2}, ${diagonalP3}`);
+determinante1 = matrizP[1][1] * matrizP[0][0]
+determinante2 = matrizP[1][0] * matrizP[0][1]
+determinante = determinante1 - determinante2
+console.log(determinante);
